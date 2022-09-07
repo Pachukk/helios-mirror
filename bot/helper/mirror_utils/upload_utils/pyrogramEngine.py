7369439797e7eb11f59f1791abdd0fd6f37a1f94
Pachukk <88023964+Pachukk@users.noreply.github.com>
2022-09-07 14:25:38 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -86,13 +86,13 @@ class TgUploader:
             leechchat = LEECH_DUMP
         else: leechchat = self.__listener.message.chat.id
         if CUSTOM_FILENAME is not None:
-            cap_mono = f"{CUSTOM_FILENAME} <b>{file_}</b>"
+            cap_mono = f"{CUSTOM_FILENAME} {file_}"
             file_ = f"{CUSTOM_FILENAME} {file_}"
             new_path = ospath.join(dirpath, file_)
             osrename(up_path, new_path)
             up_path = new_path
         else:
-            cap_mono = f"<b>{file_}</b>"
+            cap_mono = f"{file_}"
         notMedia = False
         thumb = self.__thumb
         self.__is_corrupted = False
